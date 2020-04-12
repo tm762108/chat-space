f23b26a09c87ded1aa9151b87dc3b3c474ab08e0
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-04-12 22:13:23 +0900

    make-user-controlling-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+
+>>>>>>> Stashed changes
 end
