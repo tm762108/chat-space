d2b355146430ad30df7d3dd8e1cb60aaaa0459a3
tm762108 <62545525+tm762108@users.noreply.github.com>
2020-05-03 20:48:47 +0900

    a

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,14 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
+<<<<<<< Updated upstream
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+=======
+      # binding.pry
+      respond_to do |format|
+        format.json
+      end
+>>>>>>> Stashed changes
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
